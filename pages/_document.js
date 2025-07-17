bddec615e7a96c87e90dab57faf284186b610d78
umarfarooqqq <umarfarooq63425@gmail.com>
2025-07-17 20:31:00 +0500

    changes updated

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
-      <Head>
-        <title>Umar's Portfolio</title>
-      </Head>
+      <Head></Head>
       <body className="antialiased leading-8 overflow-x-hidden">
         <Main />
         <NextScript />
